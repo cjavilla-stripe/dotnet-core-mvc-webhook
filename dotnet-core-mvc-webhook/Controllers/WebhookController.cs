b7dfe9f8538b8dc235cb0407217f0fb296fd9557
CJ Avilla <cjavilla@stripe.com>
2019-11-25 09:17:47 -0800

    Initial

---
+++ dotnet-core-mvc-webhook/Controllers/WebhookController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IO;
+using Stripe;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace dotnet_core_mvc_webhook.Controllers
+{
+    public class WebhookController : Controller
+    {
+        [HttpPost]
+        public async Task<IActionResult> IndexAsync()
+        {
+            var json = await new StreamReader(HttpContext.Request.Body).ReadToEndAsync();
+            try
+            {
+                var stripeEvent = EventUtility.ParseEvent(json);
+
+                // Handle the event
+                if (stripeEvent.Type == Events.PaymentIntentSucceeded)
+                {
+                    var paymentIntent = stripeEvent.Data.Object as PaymentIntent;
+                    Console.WriteLine(paymentIntent);
+                }
+                else if (stripeEvent.Type == Events.PaymentMethodAttached)
+                {
+                    var paymentMethod = stripeEvent.Data.Object as PaymentMethod;
+                    Console.WriteLine(paymentMethod);
+                }
+                // ... handle other event types
+                else
+                {
+                    // Unexpected event type
+                    return BadRequest();
+                }
+                return Ok();
+            }
+            catch (StripeException e)
+            {
+                return BadRequest();
+            }
+        }
+    }
+}
---
+++ dotnet-core-mvc-webhook/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace dotnet_core_mvc_webhook.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
